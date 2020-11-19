8a42486cb2f8cbce3d979c5a88f1a2b82dd2f8a0
Daniel Simonov <sk8kmd@gmail.com>
2020-11-19 20:28:52 +0300

    Add google oauth login method

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,31 +1,57 @@
-import {Button, Text, View} from "react-native";
+import {Button, Platform, Text, View} from "react-native";
 import React from "react";
 import {connect} from 'react-redux';
-import {selectorAuth} from "../store/auth/selectors";
-import {setAuthCreator} from "../store/auth/actions";
+import {selectorAuth, selectorUserInfo} from "../store/auth/selectors";
+import {setAuthCreator, setUserInfoCreator} from "../store/auth/actions";
+import * as Google from "expo-google-app-auth";
+import {androidOAuthKey} from '../../config'
 
 const mapStateToProps = (state) => (
     {
-        auth: selectorAuth(state)
+        auth: selectorAuth(state),
+        userInfo: selectorUserInfo(state)
     }
 );
 
 const mapDispatchToProps = (dispatch) => (
     {
-        setAuth: (payload) => dispatch(setAuthCreator(payload))
+        setAuth: (payload) => dispatch(setAuthCreator(payload)),
+        setUserInfo: (payload) => dispatch(setUserInfoCreator(payload))
     }
 );
 
 function AuthScreen(props) {
-    const signIn = () => {
-        props.setAuth(true);
+    const googleSignIn = async () => {
+        if (Platform.OS === 'android'){
+            const { type, accessToken, user } = await Google.logInAsync({
+                androidClientId: androidOAuthKey,
+            });
+
+            if (type === 'success') {
+                props.setAuth(true);
+                props.setUserInfo({
+                    name: user.name,
+                    photoUrl: user.photoUrl,
+                    accessToken,
+                    email: user.email
+                });
+            }
+        } else {
+            props.setAuth(true);
+            props.setUserInfo({
+                name: 'Test name',
+                photoUrl: 'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/10.png',
+                accessToken: '123sagd',
+                email: 'test@gmail.com'
+            });
+        }
     }
 
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text>Auth Screen</Text>
             <Button title="Sign in"
-                    onPress={() => signIn()}
+                    onPress={() => googleSignIn()}
             />
         </View>
     );
