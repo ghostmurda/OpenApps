50004a2cc8a800fe8b45704bab7e2a522e8ed782
Daniel Simonov <sk8kmd@gmail.com>
2020-11-20 19:34:35 +0300

    Add app card prototype

---
+++ src/components/AppCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {Avatar, Button, Card, ListItem} from "react-native-elements";
+import {Text} from "react-native";
+
+export default function AppCard(props){
+    return (
+        <Card containerStyle={{borderRadius: 10}}>
+            <ListItem containerStyle={{padding: 0, marginBottom: 12}}>
+                <Avatar size="medium" title="A" overlayContainerStyle={{backgroundColor: '#2089DC', borderRadius: 10}}/>
+                <ListItem.Content>
+                    <ListItem.Title style={{fontWeight: 'bold', fontSize: 17}}>
+                        {props.name}
+                    </ListItem.Title>
+                    <ListItem.Subtitle>
+                        {props.genre}
+                    </ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+            <Card.Divider/>
+            <Card.Image
+                style={{borderRadius: 10, minHeight: 300}}
+                source={{uri: props.img}}
+            />
+            <Text style={{marginBottom: 10, marginTop: 10, fontSize: 17}}>
+                {props.description}
+            </Text>
+            <Button
+                buttonStyle={{borderRadius: 10, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                title="Open"
+            />
+        </Card>
+    );
+}
--- src/screens/TabNavigator/AppsScreen.js
+++ src/screens/TabNavigator/AppsScreen.js
@@ -1,12 +1,22 @@
 import React from "react";
-import {Text, View} from "react-native";
+import {View} from "react-native";
 import DefaultHeader from "../../components/DefaultHeader";
+import AppCard from "../../components/AppCard";
+
+const apps = [
+    {
+        name: "App",
+        genre: "Test",
+        img: "https://app2top.ru/wp-content/uploads/2019/05/PUBG-MOBILE-0.4.0-1024x725.jpg",
+        description: "App description test text test text App description test text test text App description test text test text"
+    }
+]
 
 export default function AppsScreen(){
     return (
         <View>
             <DefaultHeader title="OpenApps pre-alpha"/>
-            <Text>AppsTab</Text>
+            {apps.map((item, i) => <AppCard key={i} {...item}/>)}
         </View>
     );
 }
