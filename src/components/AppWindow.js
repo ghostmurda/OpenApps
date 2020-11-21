f4ab9cb1f7ab873078a7595aff0c4c2a40001774
Daniel Simonov <sk8kmd@gmail.com>
2020-11-21 21:17:31 +0300

    Add react-native-webview, update some packages

--- src/components/AppWindow.js
+++ src/components/AppWindow.js
@@ -1,10 +1,15 @@
 import {Text, View} from "react-native";
+import {WebView} from 'react-native-webview';
 import React from "react";
 
 const AppWindow = ({route}) => {
     return (
-        <View>
-            <Text>App</Text>
+        <View style={{ flex: 1}}>
+            <WebView
+                originWhitelist={['*']}
+                source={{ html: '<h1>Hello world</h1>' }}
+                style={{ marginTop: 20 }}
+            />
         </View>
     );
 }
