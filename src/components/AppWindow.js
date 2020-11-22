34aaab0afbfdf326292c7aa71d7761e4e597d2b5
Daniel Simonov <sk8kmd@gmail.com>
2020-11-22 20:02:40 +0300

    Update app window component

--- src/components/AppWindow.js
+++ src/components/AppWindow.js
@@ -2,36 +2,13 @@ import {View} from "react-native";
 import {WebView} from 'react-native-webview';
 import React from "react";
 
-const HTML = `
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<h1>Hello world</h1>
-<button id="btn">Check</button>
-<script type="text/javascript">
-let a = '1';
-document.querySelector('#btn').addEventListener('click', () => {
-    alert(a);
-})
-</script>
-</body>
-</html>
-`;
-
 const AppWindow = ({route}) => {
     return (
         <View style={{flex: 1}}>
             <WebView
                 originWhitelist={['*']}
-                source={{html: HTML}}
+                source={{uri: route.params.url}}
                 javaScriptEnabled={true}
-                injectedJavaScript={`a = '2'`}
             />
         </View>
     );
