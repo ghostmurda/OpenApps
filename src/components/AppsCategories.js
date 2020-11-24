1e7b64b668c31041ae649c7bb02cebc79dce23cc
Daniel Simonov <sk8kmd@gmail.com>
2020-11-24 20:48:26 +0300

    Add selected category style and architecture

--- src/components/AppsCategories.js
+++ src/components/AppsCategories.js
@@ -1,5 +1,8 @@
 import {StyleSheet, Text, View} from "react-native";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {selectorCategory} from "../store/appsCategories/selectors";
+import {setPageCreator} from "../store/appsCategories/actions";
+import {useDispatch, useSelector} from "react-redux";
 
 export const categories = [
     {
@@ -32,11 +35,30 @@ export const categories = [
     }
 ];
 
-export const Category = ({ title }) => (
-    <View style={styles.wrapper}>
-        <Text style={styles.title}>{title}</Text>
-    </View>
-);
+export const Category = ({title}) => {
+    const [selected, setSelected] = useState(false);
+    const selectedTitle = useSelector(state => selectorCategory(state));
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (selectedTitle === title){
+            setSelected(true);
+        } else {
+            setSelected(false);
+        }
+    })
+
+    return (
+        <View style={styles.wrapper}>
+            <Text
+                style={selected ? styles.selectedTitle : styles.title}
+                onPress={() => dispatch(setPageCreator(title))}
+            >
+                {title}
+            </Text>
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
     wrapper: {
@@ -46,6 +68,11 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 17,
         lineHeight: 30,
-        color: 'gray',
+        color: 'gray'
+    },
+    selectedTitle: {
+        fontSize: 17,
+        lineHeight: 30,
+        color: '#2089DC'
     }
 });
